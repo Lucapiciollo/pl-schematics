811a9607add73288e6ca96c41e20e2619cf1ace7
Luca <lucapiciollo@gmail.com>
2020-11-18 16:15:24 +0100

    modifica http service

--- README.md
+++ README.md
@@ -1,53 +1,92 @@
+
 # Portable Library Schematics
 
-Portable Library Schematics *pl-schematics è una libreria nata allo scopo di velocizzare e standardizzare il processo di startup di un progetto angular > 2 imponendo un patter di sviluppo ottimizzato e facilmente comprensibile. 
+  
+
+Portable Library Schematics *pl-schematics è una libreria nata allo scopo di velocizzare e standardizzare il processo di startup di un progetto angular > 2 imponendo un patter di sviluppo ottimizzato e facilmente comprensibile.
+
 i progetti angular sono suddivisi, se vogliamo in 2 macro categorie.. parte core e parte grafica
-entrambe da non sottovalutare in termini di complessità e di progettualità.. non è possibile o consigliabile iniziare con gli sviluppi grafici se non si è prima pianificata la parte core, che si occupa poi di far girare tutto il sistema, quindi gia questo aspetto rende costretti un team di sviluppatori ad attendere che almeno il 90% del lavoro del core sia pronto e potrebbero volerci divese settimane, tra studio , pianificazione e messa in opera e testing. Questo template, appunto riesce a garantire uno startup immediato del progetto e mettendo a disposizione classi o servizi gia consolidati nel tempo e quindi di preoccuparsi da subito alla creazione dei moduli grafici. 
+
+entrambe da non sottovalutare in termini di complessità e di progettualità.. non è possibile o consigliabile iniziare con gli sviluppi grafici se non si è prima pianificata la parte core, che si occupa poi di far girare tutto il sistema, quindi gia questo aspetto rende costretti un team di sviluppatori ad attendere che almeno il 90% del lavoro del core sia pronto e potrebbero volerci divese settimane, tra studio , pianificazione e messa in opera e testing. Questo template, appunto riesce a garantire uno startup immediato del progetto e mettendo a disposizione classi o servizi gia consolidati nel tempo e quindi di preoccuparsi da subito alla creazione dei moduli grafici.
+
 Il risparmio di tempo è subito evidente.
+
+  
 
 ## Uso
 
-```
-1)      Creazione di un nuovo progetto
-        ng  new ProjectName
+ 
+Creazione di un nuovo progetto
+>ng new ProjectName
+>
+ installaione del pattern
+>npm i pl-schematics
 
-2)      installaione del pattern
-        npm i pl-schematics
+  
+adattamento del nuovo progetto al patter
 
-3)      adattamento del nuovo progetto al patter
-        schematics pl-schematics:pl-schematics --force
-```
-N.B
+>schematics pl-schematics:pl-schematics --force
+ 
+
 I passi descritti sono specifici per adattare un nuovo progetto al template pattern. Per adattare un progetto gia esistente, occorre prestare attenzione a clonare i seguenti file
-
-app.component.html
+ 
+ >app.component.html
 environment.ts e environment.prod.ts
-sonar-project.properties	
-app-routing.module.ts	
+sonar-project.properties
+app-routing.module.ts
+
 ## Descrizione
+
+  
 
 pl-schematics nasce da un esperienza pluriennale dove si è posta molta attenzione alle performance applicative e strategie di riusabiltà.
 
-Verra creata un'alberatura predisposta ad accogliere classi e oggetti, viene creata una sezione shared dove è possibile introdurre e condividere con tutto il resto del progetto nuove funzionalita custom non previste dal core, come componenti grafici utilità ed altro. 
+  
+
+Verra creata un'alberatura predisposta ad accogliere classi e oggetti, viene creata una sezione shared dove è possibile introdurre e condividere con tutto il resto del progetto nuove funzionalita custom non previste dal core, come componenti grafici utilità ed altro.
+
 Questo garantisce di scandire per bene il compito di ogni sezione. La parte core non deve essere manutenuta, in quanto autonoma.. per interaggire con le utiità da lei esposte, è possibile restare in ascolto di eventi da lei lanciati e registrati di default nella global.service.ts, che è una classe di servizio globale all'aplicazione e viene creata ed inizializzata dal templmate.
 
-In questo modo si evita di dover modificare la servizi o classi core. 
+  
+
+In questo modo si evita di dover modificare la servizi o classi core.
+
+  
 
 L'installazione del template, prevede l'aggiunta di lib di supporto, viene installata ad esempio la pl-core-library che è una raccolta di funzionalità che automatizzano alcuni processi e aggiungono delle nuove funzionalità come ad esempio la gestione delle stringhe degli array dei json e funzionalità come intercettazione del browser e funzionalita di rete per la gestione file e tanto altro.
 
+  
+
 Per installare il template, occorre rispondere a delle domande guida, ed in base alle risposte si avra un tipo di configurazione piuttosto che un altra
 
-È  possibile sceglire ad esempio se abilitare scanner sonar, bootstrap 4 o il supporto per l'autenticazione ad azure, al momento.
+  
+
+È possibile sceglire ad esempio se abilitare scanner sonar, bootstrap 4 o il supporto per l'autenticazione ad azure, al momento.
+
+  
 
 La sua esecuzione in un progetto nuovo, implica la creazione di diverse classi e la modifica di alcune gia presenti per garantire una corretta configurazione.
 
+  
+
 Ad un progetto gia esistente, è possibile comunque eseguire l'installazione della lib, ma occorre sapere che app.module, app.component.html file di properties, configurazione di sonar vengono sostituiti co i nuovi file.
+
+  
 
 Il sistema è alle prime versioni ne seguiranno degli aggiornamenti futuri
 
+  
+
 ## Documentazione
+
+  
 
 È possibile risalire alla documentazione di supporto presente all'interno del pacchetto. al momento dell'installazione, oltre ad adattare il progetto ospitante, verranno create alcune folder di documentazione.
 
+  
+## Release
+>Questa versione di pl-schematics, necessia della pl-core-utils-library con version ~1.2.0
+
 ## Author
+
 Created by @l.piciollo
--- src/pl-schematics/files/core/service/http.service.ts
+++ src/pl-schematics/files/core/service/http.service.ts
@@ -53,7 +53,7 @@ export class <%=classify(prefixClass)%>HttpService {
    * logtrace per visualizzare di default lo stato di avanzamento delle http
    * @param IDAjax 
    */
-    private logTraceHttp(IDAjax: any)  {
+    private logTraceHttp(IDAjax: any) {
       let trace = this.TAILAJXCALL(IDAjax).subscribe(object => {
         console.log(object)
       }, () => { }, () => { trace.unsubscribe(); })
@@ -119,8 +119,8 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  GET(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<JSON> {
-    return new Observable<JSON>(observer => {
+  GET(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.GET(this.injector.get(BASE_URL_API).concat(url), JSON.changeValues(params,null,""),responseType || RESPONSE_TYPE.JSON, PlCoreModule.Routing().getIinterrupt(), contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
         observer.next(res)
         observer.complete();
@@ -136,12 +136,12 @@ export class <%=classify(prefixClass)%>HttpService {
    * Servizio GET REST non influente al cambio di rotta.. il servizio gira in background
    * @param url          :Url BE
    * @param params       :params
-   * @param responseType :tipo di risposta RESPONSE_TYPE
+   * @param responseType :tipo di risposta RESPONSE_TYPE 
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  GETBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<JSON> {
-    return new Observable<JSON>(observer => {
+  GETBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.GET(this.injector.get(BASE_URL_API).concat(url), JSON.changeValues(params,null,""), responseType || RESPONSE_TYPE.JSON, null, contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
         observer.next(res)
         observer.complete();
@@ -163,10 +163,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  PATCH(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  PATCH(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.PATCH(this.injector.get(BASE_URL_API).concat(url), params, responseType || RESPONSE_TYPE.JSON, PlCoreModule.Routing().getIinterrupt(), contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -183,10 +183,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  PATCHBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  PATCHBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.PATCH(this.injector.get(BASE_URL_API).concat(url), params, responseType || RESPONSE_TYPE.JSON, null, contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -205,10 +205,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  POST(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  POST(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.POST(this.injector.get(BASE_URL_API).concat(url), params, responseType || RESPONSE_TYPE.JSON, PlCoreModule.Routing().getIinterrupt(), contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -225,10 +225,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  POSTBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  POSTBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.POST(this.injector.get(BASE_URL_API).concat(url), params, responseType || RESPONSE_TYPE.JSON, null, contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -245,10 +245,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  POSTFILE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  POSTFILE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.POST(this.injector.get(BASE_URL_API).concat(url), params, responseType, null, contentType, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -266,10 +266,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  GETFILE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  GETFILE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.GET(this.injector.get(BASE_URL_API).concat(url), JSON.changeValues(params,null,""), responseType, null, contentType, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -287,10 +287,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  PUT(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  PUT(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.PUT(this.injector.get(BASE_URL_API).concat(url), params, responseType, PlCoreModule.Routing().getIinterrupt(), contentType, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -307,10 +307,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  PUTBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  PUTBG(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.PUT(this.injector.get(BASE_URL_API).concat(url), params, responseType || RESPONSE_TYPE.JSON, null, contentType || null, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -327,10 +327,10 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param callBack     :funzione da lanciare al momento dell'avvio della richiesta, riceve l'id ajax per la progressbar
    * @param contentType  :tipo di contenuto ricevuto
    */
-  DELETE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<any> {
-    return new Observable<any>(observer => {
+  DELETE(url: string, params: any, responseType?: RESPONSE_TYPE, callBack?: (id: any) => void, contentType?: CONTENT_TYPE | string, mocked?: boolean): Observable<HttpResponse<any>> {
+    return new Observable<HttpResponse<any>>(observer => {
       this.plHttpService.DELETE(this.injector.get(BASE_URL_API).concat(url), params, responseType, PlCoreModule.Routing().getIinterrupt(), contentType, callBack|| this.logTraceHttp.bind(this),mocked).subscribe(res => {
-        observer.next(<JSON>res)
+        observer.next( res)
         observer.complete();
       }, err => {
         observer.error(this.checkError(err));
@@ -344,11 +344,11 @@ export class <%=classify(prefixClass)%>HttpService {
    * @param serviceUrls  :lista di url da richiamare in parallelo
    * @param params       :params 
    */
-  FORKJOIN(serviceUrls: string[]  , params: any, mocked?: boolean): Observable<any[]> { 
+  FORKJOIN(serviceUrls: string[]  , params: any, mocked?: boolean): Observable<Array<HttpResponse<any>>> { 
     serviceUrls = serviceUrls.map(URL => {
       return this.injector.get(BASE_URL_API).concat(URL);
     })
-    return new Observable<any>(observer => {
+    return new Observable<Array<HttpResponse<any>>> (observer => {
       this.plHttpService.FORKJOIN(serviceUrls, params, null,mocked).subscribe(res => {
         observer.next(res)
         observer.complete();
